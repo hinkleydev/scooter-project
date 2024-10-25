7c8c1317d1a12b48535c6199983c12343ffb7b73
hinkleydev <azz@hinkley.dev>
2024-10-25 11:35:14 +0100

    Done scooter and tests

--- __tests__/Scooter.js
+++ __tests__/Scooter.js
@@ -2,39 +2,59 @@ const { describe, expect, it } = require("@jest/globals");
 const Scooter = require("../classes/Scooter.js");
 
 describe("scooter.rent(user)", () => {
-  it.skip("checks a scooter out to a user", () => {
-    // Arrange
-    // Act
-    // Assert
+  it("checks a scooter out to a user", () => {
+    const scooter = new Scooter("ONE");
+    const user = {"username" : "default"};
+
+    scooter.rent(user);
+    expect(scooter.user).toEqual(user);
   });
 
-  it.skip("throws an error if battery dead or scooter broken", () => {
-    // Arrange
-    // Act
-    // Assert
+  it("throws an error if battery dead or scooter broken", () => {
+    const scooter = new Scooter("ONE");
+    const user = {"username" : "default"};
+
+    scooter.charge = 5;
+    expect(() => { scooter.rent(user) }).toThrow("This scooter has low charge");
+    
+    scooter.recharge() // Recharge
+    scooter.isBroken = true; // Smash it
+    expect(() => { scooter.rent(user) }).toThrow("This scooter is broken");
   });
 });
 
 describe("scooter.dock(station)", () => {
-  it.skip("returns a scooter to a station", () => {
-    // Arrange
-    // Act
-    // Assert
+  it("returns a scooter to a station", () => {
+    const scooter = new Scooter("ONE");
+    const user = {"username" : "default"};
+
+    scooter.rent(user); // Rent it out
+    scooter.dock("TWO"); // Put it back
+
+    expect(scooter.user).toBeNull(); // Check they got cleared
+    expect(scooter.station).toBe("TWO"); // Check it's in the right place
   });
 });
 
-describe("scooter.charge()", () => {
-  it.skip("charges a scooter", () => {
-    // Arrange
-    // Act
-    // Assert
+describe("scooter.recharge()", () => {
+  it("charges a scooter", () => {
+    const scooter = new Scooter("ONE");
+
+    scooter.charge = 10; // Discharge
+    scooter.recharge(); // Recharge
+
+    expect(scooter.charge).toBe(100);
   });
 });
 
 describe("scooter.repair()", () => {
-  it.skip("repairs a scooter", () => {
-    // Arrange
-    // Act
-    // Assert
+  it("repairs a scooter", () => {
+    const scooter = new Scooter("ONE");
+
+    scooter.isBroken = true; // Hulk smash!
+    expect(scooter.isBroken).toBeTruthy();
+
+    scooter.repair(); // Immortal, forever!
+    expect(scooter.isBroken).toBeFalsy();
   });
 });
--- classes/Scooter.js
+++ classes/Scooter.js
@@ -1,5 +1,72 @@
 class Scooter {
-  // Scooter code here
+  // Static
+
+  static nextSerial = 1; // Serial number for next created scooter
+
+  // Default variables
+  charge = 100;
+  isBroken = false;
+  // I made this private in the UML diagram, but in practice that won't work
+  user = null;
+  // TODO - Update UML to reflect this
+
+  /** Create a scooter, it's automatically docked at the specified station
+   * 
+   * @param {String} station 
+   */
+  constructor(station) {
+    this.station = station;
+    this.serial = Scooter.nextSerial;
+    Scooter.nextSerial++;
+  }
+  
+  /**
+   * Throws an error if the scooter is not charged enough or broken
+   */
+  #checkIfOperational() {
+    if (this.charge <= 20) { // Not enough charge
+      throw "This scooter has low charge";
+    } else if (this.isBroken) { // Damaged
+      throw "This scooter is broken";
+    }
+    return; // All is well
+  }
+
+  /**
+   * Rent a scooter out to the user
+   * @param {User} user 
+   */
+  rent(user) {
+    this.#checkIfOperational(); // This will throw an error if it fails
+    this.station = null; // Check out
+    this.user = user;
+  }
+
+  /**
+   * Name a location to dock the scooter to and remove the user
+   * @param {String} station 
+   */
+  dock(station) {
+    this.user = null;
+    this.station = station;
+  }
+
+  /**
+   * Charge scooter to 100%
+   * 
+   * It turns out a this.charge and this.charge() confuses JavaScript, I'll have to change the UML to reflect this
+   */
+  // TODO - Update UML to reflect the rename
+  recharge() {
+    this.charge = 100;
+  }
+
+  /**
+   * Repair the scooter
+   */
+  repair() {
+    this.isBroken = false;
+  }
 }
 
 module.exports = Scooter;
