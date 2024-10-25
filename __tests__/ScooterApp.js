eea9463667b6fce16365b441212fe53abedff314
hinkleydev <azz@hinkley.dev>
2024-10-25 16:17:36 +0100

    Done scooter and tests

--- __tests__/ScooterApp.js
+++ __tests__/ScooterApp.js
@@ -1,92 +1,133 @@
 const { describe, expect, it } = require("@jest/globals");
 const ScooterApp = require("../classes/ScooterApp.js");
+const User = require("../classes/User.js");
+const Scooter = require("../classes/Scooter.js");
 
 describe("ScooterApp.registerUser(username, password, age)", () => {
-  it.skip("registers a new user if old enough", () => {
-    // Arrange
-    // Act
-    // Assert
+  it("registers a new user if old enough", () => {
+    const user = new User("James1", "pa$$word", 22); // Create identical object for comparison
+    ScooterApp.registerUser("James1", "pa$$word", 22);
+
+    expect(ScooterApp.registeredUsers["James1"]).toEqual(user);
   });
 
-  it.skip("throws an error if too young or already registered", () => {
-    // Arrange
-    // Act
-    // Assert
+  it("throws an error if too young or already registered", () => {
+    expect( () => {
+      ScooterApp.registerUser("Kaite", "pa$$word", 16);
+    }).toThrow("Too young");
+
+    expect( () => {
+      ScooterApp.registerUser("James1", "password");
+    }).toThrow("User has been registered");
   });
 });
 
 describe("ScooterApp.loginUser(username, password)", () => {
-  it.skip("logs in a registered user", () => {
-    // Arrange
-    // Act
-    // Assert
+  it("logs in a registered user", () => {
+    const user = new User("James2", "pa$$word", 22); // Create identical object for comparison
+    user.login("pa$$word"); // Sets the isLoggedIn boolean to true which we need for comparison
+
+    ScooterApp.registerUser("James2", "pa$$word", 22);
+
+    ScooterApp.loginUser("James2", "pa$$word");
+    expect(ScooterApp.registeredUsers["James2"]).toEqual(user);
   });
 
-  it.skip("throws an error if user not found or password incorrect", () => {
-    // Arrange
-    // Act
-    // Assert
+  it("throws an error if user not found or password incorrect", () => {
+    expect( () => {
+      ScooterApp.loginUser("Invalid", "pa$$word");
+    }).toThrow("User not found")
+
+    ScooterApp.registerUser("James3", "pa$$word", 22);
+
+    expect( () =>{
+      ScooterApp.loginUser("James3", "passw0rd");
+    }).toThrow("Incorrect password")
   });
 });
 
 describe("ScooterApp.logoutUser(username)", () => {
-  it.skip("logs out a registered user", () => {
-    // Arrange
-    // Act
-    // Assert
+  it("logs out a registered user", () => {
+    ScooterApp.registerUser("James4", "pa$$word");
+    ScooterApp.loginUser("James4", "pa$$word"); // Create and log them in
+    ScooterApp.logoutUser("James4"); // Log out
+
+    expect(ScooterApp.registeredUsers["James4"].isLoggedIn).toBeFalsy();
   });
 
-  it.skip("throws an error if user not found", () => {
-    // Arrange
-    // Act
-    // Assert
+  it("throws an error if user not found", () => {
+    expect(() => {
+      ScooterApp.logoutUser("Invalid");
+    }).toThrow("No such user is logged in");
   });
 });
 
 describe("ScooterApp.createScooter(station)", () => {
-  it.skip("creates a new scooter and adds it to ScooterApp.stations", () => {
-    // Arrange
-    // Act
-    // Assert
+  it("creates a new scooter and adds it to ScooterApp.stations", () => {
+    const scooter = new Scooter("ONE"); // Identical object for comparison
+    scooter.serial++; // Increment the serial so the comparison passes
+    ScooterApp.createScooter("ONE");
+
+    const newScooter = ScooterApp.stations["ONE"][0];
+    expect(newScooter).toEqual(scooter);
+    scooter.serial--; // I don't think this is needed, but probably best to have anyway
   });
 
-  it.skip("throws an error if a station does not exist", () => {
-    // Arrange
-    // Act
-    // Assert
+  it("throws an error if a station does not exist", () => {
+    expect(() => {
+      ScooterApp.createScooter("FAKE");
+    }).toThrow("No such station")
   });
 });
 
 describe("ScooterApp.dockScooter(scooter, station)", () => {
-  it.skip("docks a scooter at a station", () => {
-    // Arrange
-    // Act
-    // Assert
+  it("docks a scooter at a station", () => {
+    let scooter = new Scooter("ONE");
+    scooter.rent("Test"); // Rent it out to clear the station
+
+    ScooterApp.dockScooter(scooter ,"TWO");
+    expect(
+     ScooterApp.stations["TWO"][0]
+    ).toEqual(scooter);
   });
 
-  it.skip("throws an error if a station does not exist", () => {
-    // Arrange
-    // Act
-    // Assert
+  it("throws an error if a station does not exist", () => {
+    let scooter = new Scooter("ONE");
+    scooter.rent("Test"); // Rent it out to clear the station
+
+    expect( () => {
+      ScooterApp.dockScooter(scooter, "FAKE");
+    }).toThrow("No such station");
   });
 
-  it.skip("throws an error if a scooter is already at a station", () => {
-    // Arrange
-    // Act
-    // Assert
+  it("throws an error if a scooter is already at a station", () => {
+    let scooter = new Scooter("ONE");
+    scooter.rent("Test"); // Rent it out to clear the station
+
+    ScooterApp.dockScooter(scooter, "TWO");
+    expect( () => {
+      ScooterApp.dockScooter(scooter, "TWO");
+    }).toThrow("Scooter already at station");
   });
 });
 
 describe("ScooterApp.rentScooter(scooter, user)", () => {
-  it.skip("rents a scooter out to a user", () => {
-    // Arrange
-    // Act
-    // Assert
+  it("rents a scooter out to a user", () => {
+    const user = new User("James5", "pa$$word");
+    const scooter = ScooterApp.createScooter("TWO"); // Add it to the station list
+
+    ScooterApp.rentScooter(scooter, user);
+    expect(scooter.user).toEqual(user);
   });
 
-  it.skip("throws an error if a scooter is already rented", () => {
-    // Arrange
-    // Act
-    // Assert
+  it("throws an error if a scooter is already rented", () => {
+    const scooter = ScooterApp.createScooter("THR");
+    const user = new User("James6", "pa$$word");
+    const secondUser = new User("James7", "pa$$word");
+
+    ScooterApp.rentScooter(scooter, user);
+    expect( () => {
+      ScooterApp.rentScooter(scooter, secondUser);
+    }).toThrow("Scooter already rented");
   });
 });
--- classes/ScooterApp.js
+++ classes/ScooterApp.js
@@ -2,7 +2,116 @@ const Scooter = require("./Scooter.js");
 const User = require("./User.js");
 
 class ScooterApp {
-  // ScooterApp code here
+  // TODO - Update the UML to reflect that all of these are static
+  
+  static stations = { 
+    "ONE" : [], 
+    "TWO" : [], 
+    "THR" : []
+  };
+
+  static registeredUsers = {};
+
+  /**
+   * Register a user in the app array
+   * @param {String} username 
+   * @param {String} password 
+   * @param {Number}} age 
+   */
+  static registerUser(username, password, age) {
+    const lookForUser = ScooterApp.registeredUsers[username];
+    if (lookForUser != undefined) { // If a user is found
+      throw "User has been registered";
+    }
+    const user = new User(username, password, age); // Error checking is done by the User module
+    ScooterApp.registeredUsers[username] = (user); 
+  }
+
+  /**
+   * Log one of the users of the app in
+   * @param {String} username 
+   * @param {String} password 
+   * @returns 
+   */
+  static loginUser(username, password) {
+    const user = ScooterApp.registeredUsers[username];
+    if (user == undefined) { // User not found in object
+      throw "User not found";
+    }
+    user.login(password); // Error checking is done by the User module
+    return user;
+  }
+
+  /**
+   * Log a user out of the app (does not remove them from the app)
+   * @param {String} username 
+   */
+  static logoutUser(username) {
+    let user = ScooterApp.registeredUsers[username];
+    if (user == undefined) { // User not found in object
+      throw "No such user is logged in";
+    } else if (!user.isLoggedIn) { // Not logged in
+      throw "User is logged out";
+    }
+    user.isLoggedIn = false;
+  }
+  
+  /**
+   * Check if a station is valid, throws an error if not
+   * @param {String} station 
+   */
+  static checkStation(station) {
+    let findStation = ScooterApp.stations[station];
+    if (findStation == undefined) { // If there's no station that matches
+      throw "No such station";
+    } 
+  }
+
+  /**
+   * Creates a scooter docked at the provided station
+   * @param {String} station 
+   * @returns {Scooter} 
+   */
+  static createScooter(station) {
+    ScooterApp.checkStation(station)
+    let scooter = new Scooter(station);
+    ScooterApp.stations[station].push(scooter);
+    return scooter;
+  }
+  
+  /**
+   * Rent a scooter out for a user, removing it from the station
+   * @param {Scooter} scooter 
+   * @param {User} user 
+   */
+  static rentScooter(scooter, user) { 
+    if (scooter.user != null) {
+      throw "Scooter already rented";
+    }
+    const location = scooter.station; // Where it's taken from
+    const id = scooter.serial; // Identifying that particular one
+    const stationedScooters = ScooterApp.stations[location];
+    const foundScooterIndex = stationedScooters.findIndex(function(testScooter) {
+      return id == testScooter.serial; // Compare the searched for ID with the one we're looking at
+    });
+    ScooterApp.stations[location].splice(foundScooterIndex, 1); // Delete the scooter from the station
+    scooter.rent(user); // Rent it out to the user
+  }
+
+  /**
+   * Dock a scooter at the specified station
+   * @param {Scooter} scooter 
+   * @param {String} station 
+   */
+  static dockScooter(scooter, station) {
+    ScooterApp.checkStation(station);
+    if (scooter.station == station) {
+      throw "Scooter already at station";
+    }
+    scooter.dock(station); // Automatically removes the user and sets the property
+    ScooterApp.stations[station].push(scooter);
+  }
+  
 }
 
 module.exports = ScooterApp;
