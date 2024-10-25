8a0ea54f465e8658d213d3246ff4ef04e3bb5a1f
hinkleydev <azz@hinkley.dev>
2024-10-25 10:52:40 +0100

    Done user and tests

--- __tests__/User.js
+++ __tests__/User.js
@@ -2,23 +2,32 @@ const { describe, expect, it } = require("@jest/globals");
 const User = require("../classes/User.js");
 
 describe("user.login(password)", () => {
-  it.skip("logs a user in if the password is correct", () => {
-    // Arrange
-    // Act
-    // Assert
+  it("logs a user in if the password is correct", () => {
+    const user = new User("James", "pa$$word", 22); // Uncrackable
+    user.login("pa$$word");
+
+    expect(user.isLoggedIn).toBeTruthy();
   });
 
-  it.skip("throws an error if the password is incorrect", () => {
-    // Arrange
-    // Act
-    // Assert
+  it("throws an error if the password is incorrect", () => {
+    const user = new User("James", "pa$$word", 22);
+
+    expect(() => { user.login("invalid") }).toThrow("Incorrect password");
   });
+
+  it("throws an error if a user under 18 tries to register", () => {
+    expect(() => {
+      const user = new User("Katie", "pa$$word", 16);
+    }).toThrow("Too young");
+  })
 });
 
 describe("user.logout()", () => {
-  it.skip("logs a user out", () => {
-    // Arrange
-    // Act
-    // Assert
+  it("logs a user out", () => {
+    const user = new User("James", "pa$$word", 22);
+    user.login("pa$$word"); // Login to logout
+    user.logout();
+
+    expect(user.isLoggedIn).toBeFalsy();
   });
 });
--- classes/User.js
+++ classes/User.js
@@ -1,5 +1,41 @@
 class User {
-  // User code here
+  // Start here, cause everything else depends on it
+  // A user by default is not logged in, calling the constructor creates an object of a user not logged in similar to putting them in a database
+  
+  #password; #age; // Private variables
+
+  isLoggedIn = false;
+  /**
+   * Create a user object
+   * @param {String} username 
+   * @param {String} password 
+   * @param {Number} age 
+   */
+  constructor(username, password, age) {
+    if (age < 18) {
+      throw "Too young"; // Need to be over 18
+    }
+    this.username = username; this.#password = password;
+    this.#age = age;
+  }
+
+  /**
+   * If password is correct, log user in
+   * @param {String} password 
+   */
+  login(password) {
+    if(password !== this.#password) { // If incorrect
+      throw "Incorrect password";
+    }
+    this.isLoggedIn = true;
+  }
+
+  /**
+   * Log user out
+   */
+  logout() {
+    this.isLoggedIn = false;
+  }
 }
 
 module.exports = User;
